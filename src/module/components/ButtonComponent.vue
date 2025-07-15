e714d4a916d6ee423ce82a6a4f2496f4b6c9c213
Heraldo Fortuna <heraldofortuna@gmail.com>
2025-07-14 23:53:12 -0500

    [main]: fix general design and create header, footer and button component

---
+++ src/module/components/ButtonComponent.vue
@@ -0,0 +1,12 @@
+<template>
+  <button
+    class="w-full flex items-center justify-center rounded-2xl border border-gray-500 p-6 text-center transition duration-300 ease-in-out hover:border-blue-500 hover:bg-blue-500 hover:text-white focus:border-blue-500 focus:bg-blue-500">
+    <span class="text-sm md:text-base font-medium">{{ text }}</span>
+  </button>
+</template>
+
+<script lang="ts" setup>
+defineProps<{
+  text: string;
+}>();
+</script>
